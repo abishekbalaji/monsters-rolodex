5ff83532e6a93912d6e483949f956fcfa3610a42
abishek <abishekbalaji7.151@gmail.com>
2020-10-27 10:44:29 +0530

    Updates

---
+++ precache-manifest.69fc1a5e11f5a8011509fca1e6f7c522.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e5b2923d301464adcea27662692506",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ffe785d6e5419399d1af",
+    "url": "/monsters-rolodex/static/css/main.13497a80.chunk.css"
+  },
+  {
+    "revision": "4e0c9d85c4bf991dbfb3",
+    "url": "/monsters-rolodex/static/js/2.425ecb11.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/monsters-rolodex/static/js/2.425ecb11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe785d6e5419399d1af",
+    "url": "/monsters-rolodex/static/js/main.1e5775cf.chunk.js"
+  },
+  {
+    "revision": "fc19a6298b2e6a5f9fd1",
+    "url": "/monsters-rolodex/static/js/runtime-main.d1dadcc4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
